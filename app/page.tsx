bdd7aee6d32c74b3019d7137949d7327c3a40c35
Ilona Gogiashvili <gogiashvili.ilona@gmail.com>
2026-05-04 14:02:00 -0400

    installed ShadCN and create button to test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Hi</h1>
+    <div >
+      <main>
+        <h1>Welcome to my store!</h1>
+        <Button>Welcome</Button>
       </main>
     </div>
   );
