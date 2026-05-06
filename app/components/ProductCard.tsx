a2cbfdc6e3c9dcffed7e4e34d0a27963f35878a0
Ilona Gogiashvili <gogiashvili.ilona@gmail.com>
2026-05-06 14:43:07 -0400

    test fake products on cards

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -5,11 +5,11 @@ import { Button } from "@/components/ui/button";
 export default function ProductCard({productItem}: any){
     const {name, price, description, image} = productItem;
     return (
-        <Card>
+        <Card className="w-full max-w-sm">
             <img src={image}/>
-            <h1>{name}Name of the product</h1>
-            <h1>{price}Price</h1>
-            <h1>{description}description</h1>
+            <h1>{name}</h1>
+            <h1>${price}</h1>
+            <h1>{description}</h1>
             <Button>Add to the Cart</Button>
             <Button>Add A New Product</Button>
             <Button>Delete</Button>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,22 @@
 import ProductCard from "../components/ProductCard";
+import painting from "@/public/painting.jpg";
+import clayhouse from "@/public/clayhouse.jpg"
 
 export default function ProducsPage(){
     const products=[
-        {id:1, name:"test1", price: 5, description: "Testing"},
-        {id:2, name:"test2", price: 10, description: "Testing1"},
+        {
+            id:1, 
+            name:"Painting", 
+            price: 5, 
+            description: "Painting of colors", 
+            image: "/painting.jpg"},
+        {
+            id:2, 
+            name:"Clay House", 
+            price: 10, 
+            description: "House from the clay",
+            image: "/clayhouse.jpg"
+        },
     ];
     return(
         <div>
