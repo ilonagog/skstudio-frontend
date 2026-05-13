0be5df5edc442b2e6634cb71cca3c831c9aa7854
Ilona Gogiashvili <gogiashvili.ilona@gmail.com>
2026-05-12 20:33:45 -0400

    create login form ui

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button"
-import {Card, CardAction, CardContent, CardHeader, CardTitle} from "@/components/ui/card"
+import {Card, CardAction, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 export default function  LoginPage (){
     return (
         <div>
@@ -11,8 +13,28 @@ export default function  LoginPage (){
                     </CardAction>
                 </CardHeader>
                 <CardContent>
-                    
+                    <form>
+                        <div>
+                            <div>
+                                <Label>Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <div>
+                                    <Label>Password</Label>
+                                </div>
+                                <Input id="password" type="password" required/>
+                            </div>
+                        </div>
+                    </form>
                 </CardContent>
+                <CardFooter>
+                    <Button>LogIn</Button>
+                </CardFooter>
             </Card>
         </div>
     )
